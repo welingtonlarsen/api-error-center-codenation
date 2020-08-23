2e081d149d3ef45d2b020c3ca5e13a50e8019251
Welington Larsen <larsenwelington@gmail.com>
2020-08-23 14:05:05 -0300

    Flyway sql ajusted

---
+++ src/main/resources/db/migration/postgresql/V1__init.sql
@@ -0,0 +1,3 @@
+insert into source_profile values (1, 'ADMIN');
+insert into source_profile values (2, 'SYSTEM');
+insert into source values (1, '$2a$10$3XmnZ498jk9.Cf.8vGNdm.B051a/DYuZgfHbe0QSaL/LmLLoUB4fm', 'admin', 1);
